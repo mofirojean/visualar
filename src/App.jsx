62f4de0be5b8c4d758bfbb73f46f0ae709db3aff
mofirojean <mofirojean@gmail.com>
2024-05-03 17:07:31 +0100

    (vis) implemented workflow section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar.jsx";
 import HeroSection from "./components/hero-section.jsx";
 import Feature from "./components/feature.jsx";
+import Workflow from "./components/workflow.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <div className="max-w-6xl mx-auto pt-20 px-6">
               <HeroSection />
               <Feature />
+              <Workflow />
           </div>
       </>
   )
